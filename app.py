f11347aa416362b7711ca064ad87199997e92b4f
Saotalangim161099 <Saotalangim161099@gmail.com>
2026-03-31 00:42:23 -0400

    fix: fix the path import issue in app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from app.scrapper import get_steampulse_data
 # Initialize the Flask application
 app = Flask(__name__, template_folder='templates')
 
+# Vercel needs to see 'app' as the entry point
+# We also need to help Flask find the templates if it's struggling
+app.template_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app', 'templates')
+
 # Define a route for the homepage
 @app.route('/')
 def index():
